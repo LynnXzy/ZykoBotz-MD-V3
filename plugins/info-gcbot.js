bd2178f8f0a3157dfacaf55ee41ee500ae549e41
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-12 20:13:46 +0800

    Update info-gcbot.js

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -3,18 +3,18 @@ import fetch from 'node-fetch'
  let handler = async(m, { conn }) => { 
 
          let pp = fs.readFileSync('./thumbnail.jpg')
-         let gc1 = 'https://chat.whatsapp.com/F8JCtpCaSRFLmFf70TvF3R'
+         let gc1 = 'https://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD'
          let caption = `*Mʏ Gᴄ Oғғɪᴄɪᴀʟ*`
          await conn.sendButtonDoc(m.chat, caption, wm, 'Owner', '.owner', m, { contextInfo: { 
              externalAdReply: {
                sourceUrl: `${gc1}`,
                title: 'My Group Official', 
-               body: 'Yushino Md',
+               body: 'Lynn Md',
                thumbnail: pp
              } 
          }})
          }
 
 handler.tags = ['info']
-handler.command = /^gcbul$/i
+handler.command = /^gcbot|groupbot$/i
  export default handler
